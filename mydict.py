37e8058ad71cf1bf75c57fcab55f74f31f514c26
Rotcodzjl <3509157029@qq.com>
2025-05-27 14:13:22 +0800

    last

--- mydict.py
+++ mydict.py
@@ -7,8 +7,6 @@ class Dict(dict):
             return self[key]
         except KeyError:
             raise AttributeError(r"'Dict' object has no zxy '%s'" % key)
-        try:
-            retrun 12
 
     def __setattr__(self, key, value):
         self[key] = value
